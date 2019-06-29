786a03d801a941a8de48ea7022b01895bc7ae5e1
Louka Soret <louka.soret@gmail.com>
2019-06-29 18:16:02 +0200

    enemy de ses MORTS

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -7,15 +7,14 @@ signal screen_shake(duration)
 
 export var acceleration : int = 300
 export var deceleration : int = 300
-export var top_speed : int = 200
+export var top_speed : int = 100
 export var max_health : int = 3
 
 onready var player : KinematicBody2D = get_node("/root/Game/Player")
 onready var health : int = max_health
 
 #Movement
-var accel_direction : Vector2 
-var decel_direction : Vector2
+var direction : Vector2 
 var velocity : Vector2 = Vector2()
 
 # Called when the node enters the scene tree for the first time.
@@ -23,22 +22,11 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	accel_direction = Vector2()
-	decel_direction = Vector2()
+	direction = Vector2()
 	
-	accel_direction = movement_oracle()
+	direction = movement_oracle()
 	
-	if accel_direction.x == 0:
-		decel_direction.x = (velocity.normalized()*-1).x
-	if accel_direction.y == 0:
-		decel_direction.y = (velocity.normalized()*-1).y
-	
-	velocity += acceleration*accel_direction.normalized()*delta
-	velocity += deceleration*decel_direction.normalized()*delta
-	
-	var top_directional_speed : Vector2 = velocity.normalized()*top_speed
-	velocity.x = clamp(velocity.x,0 if decel_direction.x < 0 else -abs(top_directional_speed.x),0 if decel_direction.x > 0 else abs(top_directional_speed.x))
-	velocity.y = clamp(velocity.y,0 if decel_direction.y < 0 else -abs(top_directional_speed.y),0 if decel_direction.y > 0 else abs(top_directional_speed.y))
+	velocity = top_speed*direction
 	
 	move_and_slide(velocity,Vector2.UP)
 	look_at(player.global_position)
