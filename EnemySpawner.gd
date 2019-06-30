93c6eb8af437ae772f56a3069fc6e7a222094ad8
PatteDeCanaard <>
2019-06-30 16:02:42 +0200

    m

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -5,12 +5,10 @@ signal enemy_died(position, score_gained,color)
 onready var camera : Camera2D = get_node("/root/Game/Player/Camera2D") 
 onready var terrain : Node2D = get_node("/root/Game/Terrain/Feuille")
 onready var player : KinematicBody2D = get_node("/root/Game/Player")
-
+onready var global = get_node("/root/Global")
 onready var enemy_resource : Resource = load("res://Enemies/Enemy.tscn")
 
 export var player_safe_range : int = 50
-
-var colors : Array = [Color(1,0,0),Color(0,1,0),Color(0,0,1)]
 
 func _ready():
 	randomize()
@@ -28,7 +26,7 @@ func spawn():
 		pos.x = rand_range(0,terrain.texture.get_size().x*terrain.scale.x)
 		pos.y = rand_range(0,terrain.texture.get_size().y*terrain.scale.y)
 	
-	new_enemy.color = colors[randi()%3]
+	new_enemy.color = global.colors[randi()%3]
 	
 	new_enemy.global_position = pos
 	add_child(new_enemy)
