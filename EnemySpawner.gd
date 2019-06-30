687e108280ae06df8c3c32ee81ab0c35ebfc69e8
Louka Soret <louka.soret@gmail.com>
2019-06-30 21:37:02 +0200

    ispawn enemies

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -9,14 +9,29 @@ onready var global = get_node("/root/Global")
 onready var enemy_resource : Resource = load("res://Enemies/Enemy.tscn")
 
 export var player_safe_range : int = 50
+export var max_concurent_enemies : int = 20
+export var min_concurent_enemies : int = 4
+export var wave_step : int = 5
+
+
 
 func _ready():
 	randomize()
 	
-	for i in 20:
-		spawn()
+	for i in global.colors.size():
+		for j in max_concurent_enemies/global.colors.size():
+			spawn(global.colors[i])
 
-func spawn():
+func _process(delta):
+	var child_count : int = get_child_count()
+	if get_child_count() <= min_concurent_enemies:
+		for i in global.colors.size():
+			for j in (max_concurent_enemies-min_concurent_enemies)/global.colors.size():
+				spawn(global.colors[i])
+		max_concurent_enemies += wave_step
+		min_concurent_enemies += wave_step
+
+func spawn(color : Color):
 	var pos : Vector2 = Vector2()
 	var new_enemy = enemy_resource.instance()
 	pos.x = rand_range(0,terrain.texture.get_size().x*terrain.scale.x)
@@ -26,7 +41,7 @@ func spawn():
 		pos.x = rand_range(0,terrain.texture.get_size().x*terrain.scale.x)
 		pos.y = rand_range(0,terrain.texture.get_size().y*terrain.scale.y)
 	
-	new_enemy.color = global.colors[randi()%3]
+	new_enemy.color = color
 	
 	new_enemy.global_position = pos
 	add_child(new_enemy)
