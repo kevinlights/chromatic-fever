48110bc9a292a4d49edc0b6a2bd025d98e7a2ae1
Louka Soret <louka.soret@gmail.com>
2019-07-20 18:45:59 +0200

    menu plus joli !

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -3,8 +3,11 @@ extends Control
 
 func _ready():
 	$VBoxContainer/NewGameButton.connect("pressed",self,"_on_new_game_button_entered")
-	$VBoxContainer/TutorialButton.connect("pressed",self,"_on_tutorial_button_entered")
 	$VBoxContainer/QuitButton.connect("pressed",self,"_on_quit_button_entered")
+	$VBoxContainer/NewGameButton.connect("mouse_entered",self,"_on_newgame_button_hovered")
+	$VBoxContainer/QuitButton.connect("mouse_entered",self,"_on_quit_button_hovered")
+	$VBoxContainer/NewGameButton.connect("mouse_exited",self,"_on_button_unhovered")
+	$VBoxContainer/QuitButton.connect("mouse_exited",self,"_on_button_unhovered")
 
 func _on_new_game_button_entered():
 	get_tree().change_scene("res://Game.tscn")
@@ -14,6 +17,17 @@ func _on_tutorial_button_entered():
 
 func _on_quit_button_entered():
 	get_tree().quit()
+	
+func _on_newgame_button_hovered():
+	$Hand.show()
+	$Hand.rect_position = Vector2(170,290)
+	
+func _on_quit_button_hovered():
+	$Hand.show()
+	$Hand.rect_position = Vector2(170,420)
+	
+func _on_button_unhovered():
+	$Hand.hide()
 	
 func _input(event):
 	if event.is_action_pressed("ui_cancel") and $TutorialPanel.visible:
